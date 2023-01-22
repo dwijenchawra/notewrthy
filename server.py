117b97e42d280a53bc92130b4b25c1c52b6586c5
Dwijen Chawra <dwchawra@gmail.com>
2023-01-21 20:22:53 -0800

    pushing for later

--- server.py
+++ server.py
@@ -1,25 +1,44 @@
 import os
-from time import time
+import time
 import psycopg2
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template, redirect
 from pydub import AudioSegment
 from google.cloud import speech
 import nltk
+from cryptography.fernet import Fernet
 from nltk.sentiment import SentimentIntensityAnalyzer
+import dotenv
 
-conn = psycopg2.connect(os.environ["DATABASE_URL"])
+from spotify.test import SpotifyClient
+
+
+dotenv.load_dotenv()
+
+conn = psycopg2.connect(os.getenv("DATABASE_URL"), sslmode='require')
 app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     response = startup.getUser()
+#     return redirect(response)
+
+@app.route('/callback/')
+def callback():
+    test = SpotifyClient()
+    return render_template("http://localhost:5000/callback/")
+
+
+
 
 @app.route("/create_playlist")
 def create_playlist():
     pass
 
-@app.route("/")
-def index():
-    return {"owijefw": "owiefj"}
-
 @app.route("/sign_up", methods=["POST"])
 def sign_up():
+    with open("../aes-128.key") as f:
+        line = f.readline()
+    print(line)
     return jsonify("success")
     
 
@@ -75,36 +94,56 @@ def analyze_audio():
 
 if __name__ == "__main__":
     # creating the table
-    with conn.cursor() as cur:
+    # with conn.cursor() as cur:
  
         # cur.execute("CREATE TABLE preferences (user_id UUID, top5Genres STRING[], top5Artists STRING[], top5Songs STRING[], );")
 
-        # make user_id in preferences a unique key
-        cur.execute("ALTER TABLE preferences ADD CONSTRAINT user_id UNIQUE (user_id);")
+        #drop preferences table
+        # cur.execute("DROP TABLE IF EXISTS preferences;")
 
-        cur.execute("CREATE TABLE songs (song_id STRING, song_data JSONB, valence FLOAT, energy FLOAT, danceability FLOAT, tempo FLOAT, mode FLOAT, acousticness FLOAT, instrumentalness FLOAT, loudness FLOAT, preferences_id UUID, FOREIGN KEY (preferences_id) REFERENCES preferences(user_id));")
-        # make valence, energy, danceability, tempo, mode, acousticness, instrumentalness, loudness indexes
-        cur.execute("CREATE INDEX valence_index ON users (valence);")
-        time.sleep(2)
-        cur.execute("CREATE INDEX energy_index ON users (energy);")
-        time.sleep(2)
-        cur.execute("CREATE INDEX danceability_index ON users (danceability);")
-        time.sleep(2)
-        cur.execute("CREATE INDEX tempo_index ON users (tempo);")
-        time.sleep(2)
-        cur.execute("CREATE INDEX mode_index ON users (mode);")
-        time.sleep(2)
-        cur.execute("CREATE INDEX acousticness_index ON users (acousticness);")
-        time.sleep(2)
-        cur.execute("CREATE INDEX instrumentalness_index ON users (instrumentalness);")
-        time.sleep(2)
-        cur.execute("CREATE INDEX loudness_index ON users (loudness);")
-        time.sleep(2)
+        # # make user_id in preferences a unique key and primary
+        # cur.execute("CREATE TABLE preferences (user_id UUID PRIMARY KEY, top5Genres STRING[], top5Artists STRING[], top5Songs STRING[]);")
+
+        # # time.sleep(20)
 
 
+        # cur.execute("CREATE TABLE songs (song_id STRING, song_data JSONB, valence FLOAT, energy FLOAT, danceability FLOAT, tempo FLOAT, mode FLOAT, acousticness FLOAT, instrumentalness FLOAT, loudness FLOAT, preferences_id UUID, FOREIGN KEY (preferences_id) REFERENCES preferences(user_id));")
+        # # make valence, energy, danceability, tempo, mode, acousticness, instrumentalness, loudness indexes
+        # cur.execute("CREATE INDEX valence_index ON songs (valence);")
+        # time.sleep(2)
+        # cur.execute("CREATE INDEX energy_index ON songs (energy);")
+        # time.sleep(2)
+        # cur.execute("CREATE INDEX danceability_index ON songs (danceability);")
+        # time.sleep(2)
+        # cur.execute("CREATE INDEX tempo_index ON songs (tempo);")
+        # time.sleep(2)
+        # cur.execute("CREATE INDEX mode_index ON songs (mode);")
+        # time.sleep(2)
+        # cur.execute("CREATE INDEX acousticness_index ON songs (acousticness);")
+        # time.sleep(2)
+        # cur.execute("CREATE INDEX instrumentalness_index ON songs (instrumentalness);")
+        # time.sleep(2)
+        # cur.execute("CREATE INDEX loudness_index ON songs (loudness);")
+        # time.sleep(2)
 
-        conn.commit()
-        cur.execute("SHOW TABLES;")
-        res = cur.fetchall()
-        print(res)
-    # app.run(host="0.0.0.0", debug=True)
+
+
+        # conn.commit()
+        # cur.execute("SHOW TABLES;")
+        # res = cur.fetchall()
+        # print(res)
+    # fern = Fernet(key)
+    # message = "This is the password"
+    # encMessage = fernet.encrypt(message.encode())
+    # print("original string: ", message)
+    # print("encrypted string: ", encMessage)
+    
+    # # decrypt the encrypted string with the
+    # # Fernet instance of the key,
+    # # that was used for encrypting the string
+    # # encoded byte string is returned by decrypt method,
+    # # so decode it to string with decode methods
+    # decMessage = fernet.decrypt(encMessage).decode()
+    
+    # print("decrypted string: ", decMessage)
+    app.run(host="0.0.0.0", debug=True)

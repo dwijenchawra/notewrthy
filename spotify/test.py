765aee44b345afb41346616e33b0119357caa2e6
Dwijen Chawra <dwchawra@gmail.com>
2023-01-21 20:38:38 -0500

    recommendations

--- spotify/test.py
+++ spotify/test.py
@@ -9,35 +9,142 @@ import os
 
 load_dotenv()
 
-client_credentials_manager = SpotifyClientCredentials(client_id=os.getenv('SPOTIFY_CLIENT_ID'), client_secret=os.getenv('SPOTIFY_CLIENT_SECRET'))
-sp = spotipy.Spotify(
-    client_credentials_manager=client_credentials_manager,
-    auth_manager=SpotifyOAuth(
-        client_id=os.getenv('SPOTIFY_CLIENT_ID'),
-        client_secret=os.getenv('SPOTIFY_CLIENT_SECRET'),
-        redirect_uri=os.getenv('SPOTIFY_REDIRECT_URI'),
-        scope='user-read-recently-played user-library-read playlist-modify-private playlist-modify-public user-library-read user-library-modify user-top-read user-read-currently-playing',
-        cache_path=os.getenv('SPOTIFY_CACHE_PATH'))
 
-    )
 
-search_str = 'Muse'
-pprint(sp.current_user_recently_played())
+class SpotifyClient:
 
-pprint(sp.current_user())
+    def __init__(self):
+        client_credentials_manager = SpotifyClientCredentials(client_id=os.getenv('SPOTIFY_CLIENT_ID'), client_secret=os.getenv('SPOTIFY_CLIENT_SECRET'))
+        self.client = spotipy.Spotify(
+            client_credentials_manager=client_credentials_manager,
+            auth_manager=SpotifyOAuth(
+                client_id=os.getenv('SPOTIFY_CLIENT_ID'),
+                client_secret=os.getenv('SPOTIFY_CLIENT_SECRET'),
+                redirect_uri=os.getenv('SPOTIFY_REDIRECT_URI'),
+                scope='user-read-recently-played user-library-read playlist-modify-private playlist-modify-public user-library-read user-library-modify user-top-read user-read-currently-playing',
+                cache_path=os.getenv('SPOTIFY_CACHE_PATH'))
+            )
 
-username = sp.current_user()["id"]
+    def get_current_user(self):
+        return self.client.current_user()
+    
+    def get_current_user_recently_played(self, limit=5):
+        return self.client.current_user_recently_played(limit)
+    
+    def get_current_user_top_tracks(self, limit=5):
+        return self.client.current_user_top_tracks(limit, time_range='short_term')
+    
+    def get_current_user_top_artists(self, limit=5):
+        return self.client.current_user_top_artists(limit, time_range='short_term')
+    
+    def get_current_user_saved_tracks(self):
+        return self.client.current_user_saved_tracks(limit=1000)
+    
+    def get_track_name(self, track_id):
+        return self.client.track(track_id)['name']
 
-# # Get the user's top songs
-# top_songs = sp.current_user_top_tracks()
-# song_uris = [song['uri'] for song in top_songs['items']]
 
-# # Create a new playlist for the user's top songs
-# playlist_name = "My Top Songs"
-# playlist = sp.user_playlist_create(username, playlist_name)
-# playlist_id = playlist['id']
+    def recommend_song(self, emotion):
+        user_id = self.get_current_user()['id']
+        if emotion == "happy":
+            valence = 0.8
+            energy = 0.6
+            danceability = 0.8
+            tempo = 120
+        elif emotion == "sad":
+            valence = 0.2
+            energy = 0.4
+            danceability = 0.4
+            tempo = 80
+        elif emotion == "love":
+            valence = 0.7
+            energy = 0.5
+            danceability = 0.7
+            tempo = 100
+        elif emotion == "excited":
+            valence = 0.7
+            energy = 0.7
+            danceability = 0.8
+            tempo = 140
+        elif emotion == "angry":
+            valence = 0.4
+            energy = 0.8
+            danceability = 0.6
+            tempo = 140
+        else:
+            print("Invalid emotion")
+            return
 
-# # Add the songs to the playlist
-# sp.user_playlist_add_tracks(username, playlist_id, song_uris)
+        # results = self.get_current_user_saved_tracks()
+        results = self.client.current_user_saved_tracks(limit=50, offset=0)
+        temp = results['items']
+        # while results['next']:
+        #     results = self.client.next(results)
+        #     temp.extend(results['items'])
 
-# print("Successfully created playlist " + playlist_name + " with your top songs.")
+        
+        tracks = []
+        for item in temp:
+            tracks.append(item['track']['id'])
+        
+        print(len(tracks))
+
+        features = self.client.audio_features(tracks)
+        trackdict = {self.get_track_name(tracks[i]): features[i] for i in range(len(tracks))}
+
+        print(len(features))
+
+        recommended_tracks = {}
+        
+
+        for element in trackdict.keys():
+            trackfeatures = trackdict[element]
+            if emotion == "happy":
+                if trackfeatures['valence'] > valence and trackfeatures['energy'] > energy and trackfeatures['danceability'] > danceability and trackfeatures['tempo'] > tempo:
+                    recommended_tracks[element] = trackfeatures
+            elif emotion == "sad":
+                if trackfeatures['valence'] < valence and trackfeatures['energy'] < energy and trackfeatures['danceability'] < danceability and trackfeatures['tempo'] < tempo:
+                    recommended_tracks[element] = trackfeatures
+            elif emotion == "love":
+                if trackfeatures['valence'] > valence and trackfeatures['energy'] < energy and trackfeatures['danceability'] > danceability and trackfeatures['tempo'] < tempo:
+                    recommended_tracks[element] = trackfeatures
+            elif emotion == "excited":
+                if trackfeatures['valence'] > valence and trackfeatures['energy'] > energy and trackfeatures['danceability'] > danceability and trackfeatures['tempo'] > tempo:
+                    recommended_tracks[element] = trackfeatures
+            elif emotion == "angry":
+                if trackfeatures['valence'] < valence and trackfeatures['energy'] > energy and trackfeatures['danceability'] < danceability and trackfeatures['tempo'] > tempo:
+                    recommended_tracks[element] = trackfeatures
+            else:
+                print("Invalid emotion")
+                return
+        if recommended_tracks:
+            #return 5 recommended songs
+            return recommended_tracks
+        else:
+            return None
+
+
+
+        
+
+
+test = SpotifyClient()
+print(test.get_current_user()['display_name'])
+print("-----------------------")
+for item in test.get_current_user_recently_played()['items']:
+    print(item['track']['name'])
+print("-----------------------")
+for item in test.get_current_user_top_tracks()['items']:
+    print(item['name'])
+print("-----------------------")
+for item in test.get_current_user_top_artists()['items']:
+    print(item['name'])
+print("-----------------------")
+
+# for item in test.get_current_user_saved_tracks()['items'][:5]:
+    # print(item['track']['name'])
+
+print("-----------------------")
+
+print(test.recommend_song("sad"))
+
